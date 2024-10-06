0bab23fbc05b58f7701f929d9268ce79e8d7fa2f
shin-34 <shinmegg@gmail.com>
2024-10-06 09:14:04 +0900

    質問用コミット

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,9 @@ require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
+});
+
+Route::middleware(['auth', 'can:admin-access'])->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
+    Route::get('/admin/users/{id}', [UserController::class, 'show'])->name('admin.users.show');
 });
